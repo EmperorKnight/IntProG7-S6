550e9626fc8fbbdbb3871b92afbc608dfbabf402
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-30 13:36:49 -0600

    Modificado el ejercicio 4 de la guia didactica de la semana 6

--- Actividad_3/Ejercicio_4.py
+++ Actividad_3/Ejercicio_4.py
@@ -7,9 +7,8 @@ def main():
     os.system("cls || clear")
     numero = int(input(f"Introduzca un numero \n-> "))
     while numero >= 0:
-        print(f"\rConteo regresivo: {numero}          ",end = "")
+        print(f"\rConteo regresivo: {numero}  ",end = "")
         time.sleep(1)
         numero -= 1
-    print("Fin de la cuenta regresiva")
-
+    print(f"\nFin de la cuenta regresiva")
 main()
